a88fc1c95326ebb2c3d831228a012327ab23cc74
JolitaAG <jolita278@gmail.com>
2017-07-25 14:12:13 +0300

    added data array for buttons

---
+++ js/data.js
@@ -0,0 +1,22 @@
+var buttons_data =  [
+    {"type": "number", "name":"1", "value":"1", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"2", "value":"2", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"3", "value":"3", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"4", "value":"4", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"5", "value":"5", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"6", "value":"6", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"7", "value":"7", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"8", "value":"8", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"9", "value":"9", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"+-", "value":"+-", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"0", "value":"0", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":".", "value":".", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"-", "value":"-", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"/", "value":"/", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"*", "value":"*", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"+", "value":"+", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"%", "value":"%", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"C", "value":"C", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"<|", "value":"<|", "class":"btn btn-default btn-responsive"},
+    {"type": "number", "name":"=", "value":"=", "class":"btn btn-default btn-responsive"}
+];
