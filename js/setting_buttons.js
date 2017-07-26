3c83af43f69cb60c810451b36e122f0e7ec25086
JolitaAG <jolita278@gmail.com>
2017-07-26 11:20:06 +0300

    edited button click f-tion,

--- js/setting_buttons.js
+++ js/setting_buttons.js
@@ -12,22 +12,24 @@ $('<input/>').attr({
 
 $(document).ready(function () {
     var divrow = $("<div/>").attr('class', 'calc-row').appendTo('.container');
+
     $.each(buttons_data, function (index, buttons_data) {
-        divrow.append($("<button onclick='handle_click()'>" + buttons_data.value + '</button>').attr("type", buttons_data.type).attr("name", buttons_data.name).attr("value", buttons_data.value).attr("class", buttons_data.class));
+        divrow.append($("<button>" + buttons_data.value + '</button>')
+            .attr("type", buttons_data.type)
+            .attr("name", buttons_data.value)
+            .attr("value", buttons_data.value)
+            .attr("class", buttons_data.class));
     });
+
     $("#container").html(divrow);
 
 });
 
 var res = '';
-function handle_click() {
-    $(document).ready(function () {
-        $(".btn-default").click(function () {
-            var value = $(this).val();
-             $('#input').val(res+= value);
-            //$('#input').attr('value', $(this).val());
+$(document).ready(function () {
+    $(".btn-default").click(function () {
+        var value = $(this).val();
+        $('#input').val(res+= value);
 
-
-        });
     });
-}
+});
